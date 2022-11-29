b6e18ab19daef6d4960eeb42a30f7a71951dec2b
Akshay Manthekar <58980814+Akshay4452@users.noreply.github.com>
2022-11-29 12:17:30 +0530

    VCAM added to follow the car

--- Simple Driving/Assets/Scripts/Car.cs
+++ Simple Driving/Assets/Scripts/Car.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private float acceleration = 2.0f;
+    [SerializeField] private float turnSpeed = 200f;
+    private int steerValue;
+    
     // Update is called once per frame
     void Update()
     {
         // Acceleration code
         speed += acceleration * Time.deltaTime;
-        
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    public void Steer(int value)
+    {
+        steerValue = value;
+        // transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
     }
 }
